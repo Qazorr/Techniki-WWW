402968c0c86dac05d5df507c66b93c38de04eaad
Qazorr <kpiatkowski223@gmail.com>
2022-11-07 12:43:06 +0100

    v1.5 fixed scroll top showing in smaller sizes

--- 1/js/top-page.js
+++ 1/js/top-page.js
@@ -1,7 +1,5 @@
-// Get the button:
 let mybutton = document.getElementById("top");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
     scrollFunction();
 };
@@ -16,9 +14,7 @@ function scrollFunction() {
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.documentElement.scrollTop = 0;
     mybutton.style.opacity = "0";
 }
